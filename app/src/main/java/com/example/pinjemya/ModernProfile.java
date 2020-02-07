5ef59bd5d170e632d999d6667e809006f922c933
Bobi Mokodompit <obing311200@gmail.com>
2020-02-07 09:25:02 +0700

    Share Project Pertama Kali

---
+++ app/src/main/java/com/example/pinjemya/ModernProfile.java
@@ -0,0 +1,14 @@
+package com.example.pinjemya;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ModernProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.modern_profile);
+    }
+}
